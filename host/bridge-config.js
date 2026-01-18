82bec8a6e69c95250c484f1960f696deafb882e9
conigliaro <verenzuela_6@hotmail.com>
2026-01-18 14:27:59 +0100

    Allow host

--- host/bridge-config.js
+++ host/bridge-config.js
@@ -2,5 +2,6 @@ export const ALLOWED_ORIGINS = [
   'https://mybudgetsocial.com',
   'https://staging.mybudgetsocial.com',
   'http://localhost:5173',
+  'https://financialtransactionscontrol.netlify.app'
 ];
 
